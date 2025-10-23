96df47be45b2704b153a1a8ea41d8109ac4df886
Daniel Mikes <max8@post.cz>
2025-10-23 09:05:12 +0200

    refactor

---
+++ REFACTOR.md
@@ -0,0 +1,6 @@
+mv app.py app-x/__init__.py
+mv core.py app-x/core.py
+mv static/ app-x/
+mv templates/ app-x/
+mv app-x app
+mv app-x/core-x app/core
---
+++ script/cache_clear.sh
@@ -0,0 +1,2 @@
+find . -type d -name "__pycache__" -exec rm -r {} +
+rm -rf /Users/dan/Library/Caches/BraveSoftware/Brave-Browser/Default/Cache/Cache_Data/*.*
