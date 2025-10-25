2c0fada954544e78c21a2bf238952f971a2b5f97
Daniel Mikes <max8@post.cz>
2025-10-25 10:41:55 +0200

    layout

--- app/route.py
+++ app/route.py
@@ -5,3 +5,7 @@ home = Blueprint('home', __name__, static_folder='.', template_folder='.')
 @home.route('/')
 def index():
   return render_template('index.htm')
+
+@home.route('/help')
+def help():
+  return render_template('help.htm')
